07c9d768cc327cb41423bfa853622083fb5488ca
Vinícius Mendes Lopes <69206@PROWAY.TREINA>
2020-09-05 17:17:18 -0300

    3.1 - trabalhando com variaveis

--- App-java/src/br/com/fundamentos/TrabalhandoComVariaveis.java
+++ App-java/src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -1,6 +1,9 @@
 package br.com.fundamentos;
 
 public class TrabalhandoComVariaveis {
+	/*
+	 * https://medium.com/@mauriciogeneroso/java-oca-1z0-808-2-1-tipos-de-dados-vari%C3%A1veis-vari%C3%A1veis-primitivas-e-de-refer%C3%AAncia-c8f007756bc5
+	 */
 	
 	public static void main(String[] args) {
 		int quantidade; //Declarando variavel inteiro
@@ -11,6 +14,16 @@ public class TrabalhandoComVariaveis {
 		quantidade = 15;
 		
 		System.out.println(quantidade);
+		
+		double numeroQuebrado = 1.3;
+		System.out.println("Numero decimal -> " + numeroQuebrado);
+		
+		float numeroQuebradoPontoFloat = 1.5f;
+		System.out.println("Numero decimal com float -> " + numeroQuebradoPontoFloat);
+		
+		long numeroInteiroGrande = 25l;
+		System.out.println("Numero inteiro com long -> " + numeroInteiroGrande);
+		
 	}
 
 }
