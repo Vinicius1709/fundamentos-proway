48b39b191b3e4f9034d507cd15a390116fd3981d
Vinícius Mendes Lopes <69206@PROWAY.TREINA>
2020-09-05 17:45:51 -0300

    4 - nomeando variaveis

---
+++ App-java/src/br/com/fundamentos/NomeandoVariaveis.java
@@ -0,0 +1,35 @@
+package br.com.fundamentos;
+
+public class NomeandoVariaveis {
+	
+	public static void main(String[] args) {
+		// validp separado por underline _
+		String variavel_seprada_com_underline;
+		
+		//padrao camelCase
+		String variavelJuntaComCamelCase;
+		
+		// valido porem estranho
+		int _numero = 1;
+		
+		System.out.println(_numero);
+		
+		//valido porem mais estranho ainda! Parece PHP
+		int $outroNumero = 1;
+		
+		System.out.println($outroNumero);
+		
+		/*
+		 *  nao pode iniciar com caracteres especiais
+		
+		String 1um;
+		
+		int @arroba;
+		
+		String *texto
+		
+		 */
+		
+	}
+
+}
